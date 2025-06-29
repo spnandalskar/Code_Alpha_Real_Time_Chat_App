d486fbeec78a0636abf8346405f3e9481e3922c7
spnandalskar <shwetanandalskar@gmail.com>
2025-06-29 20:35:02 +0530

    bug fixed message.route.jsx

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -8,6 +8,10 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 
-router.post("/send/:id", protectRoute, sendMessage)
+router.post("/send/:id", protectRoute, sendMessage);
+
+router.get("/", (req, res) => {
+    res.status(400).json({ error: "Message ID is required in URL" });
+});
 
 export default router;
