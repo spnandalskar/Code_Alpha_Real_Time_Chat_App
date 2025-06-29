6015f41244784088435c17d4bd8cc72cedead550
spnandalskar <shwetanandalskar@gmail.com>
2025-06-29 19:45:57 +0530

    bug fixed useAuthStore

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -84,13 +84,14 @@ export const useAuthStore = create((set, get) => ({
 
     connectSocket: () => {
         const { authUser } = get();
-        if (!authUser || get().socket?.connected) return;
+        if (!authUser || !authUser._id || get().socket?.connected) return;
 
         const socket = io(BASE_URL, {
             query: {
                 userId: authUser._id,
             },
         });
+
         socket.connect();
 
         set({ socket: socket });
