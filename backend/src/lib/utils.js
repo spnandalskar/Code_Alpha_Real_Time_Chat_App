496ee4c39b7ec91272d744553b35c86a5fedfe81
spnandalskar <shwetanandalskar@gmail.com>
2025-06-29 21:29:55 +0530

    final bug

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,12 +4,14 @@ export const generateToken = (userId, res) => {
 
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
+    const isProd = process.env.NODE_ENV === "production";
+
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, //MS
-        httpOnly: true, //prevent XSS attacks cross-site scripting attacks
-        sameSite: "strict", //CSRF attacks cross-site request forgery attacks
-        secure: process.env.NODE_ENV !== "development",
-    })
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true,
+        secure: isProd, // ❗️true only in production
+        sameSite: isProd ? "None" : "Lax", // ❗️'None' allows cross-site cookies
+    });
 
     return token;
 } 
