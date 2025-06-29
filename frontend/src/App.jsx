c734fa1a3ef843018e566ddf21eb418226f791cf
spnandalskar <shwetanandalskar@gmail.com>
2025-06-29 13:00:41 +0530

    buf fixed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
